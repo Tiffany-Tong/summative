6009ebaeb07d9e65a93e02eba089b5da813a1ea4
[Tiffany-Tong] <tiffanyxtong@gmail.com>
2023-01-12 11:30:57 -0500

    added another path and migrated A5

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
-<script></script>
+<script setup>
+import SiteHeader from "../components/SiteHeader.vue";
+import SiteHero from "../components/SiteHero.vue";
+import SiteFooter from "../components/SiteFooter.vue";
+</script>
 
-<template></template>
+<template>
+  <div class="home-container">
+    <SiteHeader />
+    <SiteHero />
+    <SiteFooter />
+  </div>
+</template>
 
-<style></style>
+<style scoped>
+.home-container {
+  /* display: flex;
+  flex-direction: row;
+  align-items: start;
+  justify-content: start; */
+  width: 100vw;
+  height: 100vh;
+}
+</style>
